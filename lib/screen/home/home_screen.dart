51fffcaae892f5ce76b2cb3db0da27b2ff06160e
ShahinAlamK <kerons89@gmail.com>
2023-09-19 10:15:12 +0600

    home cng

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -24,31 +24,35 @@ class _HomeScreenState extends State<HomeScreen> {
 
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: getW(10)),
-        child: Column(
-          children: [
-            SizedBox(height: getH(15)),
-            const TopSlider(),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              SizedBox(height: getH(15)),
+              const TopSlider(),
 
-            SizedBox(height: getH(20)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Enrolled",style:AppFont.large.copyWith(fontSize: getW(14),color:AppColor.font),),
-                Text("See more",style:AppFont.regular.copyWith(fontSize: getW(14),color:AppColor.font),),
-              ],
-            ),
-            SizedBox(height: getH(10)),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children:List.generate(10, (index) => const Padding(
-                  padding: EdgeInsets.only(right: 10),
-                  child: SpecialCard(),
-                )),
+              SizedBox(height: getH(20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Enrolled Course",style:AppFont.large.copyWith(fontSize: getW(14),color:AppColor.font),),
+                  Text("See more",style:AppFont.regular.copyWith(fontSize: getW(14),color:AppColor.font),),
+                ],
               ),
-            )
+              SizedBox(height: getH(10)),
+              const Enrolled(),
 
-          ],
+              SizedBox(height: getH(20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Live Course",style:AppFont.large.copyWith(fontSize: getW(14),color:AppColor.font),),
+                  Text("See more",style:AppFont.regular.copyWith(fontSize: getW(14),color:AppColor.font),),
+                ],
+              ),
+
+            ],
+          ),
         )
       ),
     );
@@ -78,8 +82,25 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class Enrolled extends StatelessWidget {
+  const Enrolled({
+    super.key,
+  });
 
-
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children:List.generate(10, (index) => const Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: SpecialCard(),
+        )),
+      ),
+    );
+  }
+}
 
 
 class SpecialCard extends StatelessWidget {
@@ -89,38 +110,36 @@ class SpecialCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: getH(130),
-      width: getW(120),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(getW(5)),
-        color: Colors.blueGrey.shade100,
-        image: const DecorationImage(
-          fit: BoxFit.cover,
-            image: NetworkImage("https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS2yCTSl4jxrRgdAa_7ron5792IkrWZyY53AdUQU6zeAPO98UsPJ4_i8e1uqT4jxSa9kZf7p61yhyQwSo8"))
-      ),
-      child:Container(
-        decoration: BoxDecoration(
-          gradient:LinearGradient(colors:[
-            Colors.black.withOpacity(.6),
-            Colors.grey.withOpacity(.1),
-          ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter
+    return SizedBox(
+      width: 120,
+      child: Column(
+        children: [
+          Container(
+            height: getH(100),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(getW(5)),
+              color: Colors.blueGrey,
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                  image: NetworkImage("https://cdn.ostad.app//course/photo/2023-03-29T06-44-24.054Z-flutter%20main%20course.jpeg"))
+            ),
           ),
-          borderRadius: BorderRadius.circular(getW(5))
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(getW(5)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Batch 4",style: AppFont.regular.copyWith(color: Colors.white,fontSize:getW(10)),),
-              Text("App Development with Flutter",style: AppFont.regular.copyWith(color: Colors.white,fontSize:getW(10)),),
-            ],
-          ),
-        ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: getW(5),vertical: getH(5)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Batch 4",style: AppFont.regular.copyWith(fontSize:getW(10)),),
+                SizedBox(height: getH(5)),
+                Text("App Development with Flutter",maxLines: 2,
+                  style: AppFont.medium.copyWith(fontSize:getW(9)),),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
