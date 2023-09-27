c03afc624a85740ce983184f49f67098fdadab54
ShahinAlamK <kerons89@gmail.com>
2023-09-27 16:58:01 +0600

    home cng

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:practice/style/color.dart';
 import 'package:practice/style/fonts.dart';
 import 'package:practice/style/size_config.dart';
-import 'package:practice/utils/app_string.dart';
-import 'package:svg_flutter/svg_flutter.dart';
-import '../../component/round_button.dart';
-import 'top_slider.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -16,216 +13,142 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final _controller=TextEditingController();
+  final key=GlobalKey<FormState>();
+  List<Map<String,dynamic>> todoList=[];
+
+  void addTodo(){
+    if(key.currentState!.validate()){
+      if(_controller.text.isNotEmpty){
+        Map<String,dynamic>data={
+          "title":_controller.text,
+          "date":DateTime.now(),
+        };
+        todoList.add(data);
+        _controller.clear();
+        setState(() {
+
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:AppColor.primary,
-      appBar: buildAppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text("Todo Application"),
+      ),
+      
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        todoList.clear();
+        setState(() {
+
+        });
+      },child:const Icon(Icons.clear),),
 
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: getW(10)),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              SizedBox(height: getH(15)),
-              const TopSlider(),
+        padding: EdgeInsets.symmetric(horizontal: getW(20)),
+        child: Column(
+          children: [
+            SizedBox(height: getH(10)),
 
-              SizedBox(height: getH(20)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            SizedBox(
+              height:70,
+              width: double.infinity,
+              child: Row(
                 children: [
-                  Text("Enrolled Course",style:AppFont.large.copyWith(fontSize: getW(14),color:AppColor.font),),
-                  Text("See more",style:AppFont.regular.copyWith(fontSize: getW(14),color:AppColor.font),),
+                  Expanded(
+                    child: Form(
+                      key: key,
+                      child: TextField(
+                        controller:_controller,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          filled: true,
+                          fillColor: Colors.grey.shade100,
+                          contentPadding: EdgeInsets.symmetric(horizontal: getW(15),vertical:getH(16)),
+                          hintText: "Something write...",
+
+                        ),
+                      ),
+                    ),
+                  ),
+                  
+                  SizedBox(width: getW(15)),
+                  
+                  SizedBox(
+                    width: getW(80),
+                    height: getH(45),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shadowColor: Colors.transparent
+                      ),
+                        onPressed: (){addTodo();},
+                        child:const Text("Add")),
+                  ),
+
+
                 ],
               ),
-              SizedBox(height: getH(10)),
-              const Enrolled(),
+            ),
 
-              SizedBox(height: getH(20)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Live Course",style:AppFont.large.copyWith(fontSize: getW(14),color:AppColor.font),),
-                  Text("See more",style:AppFont.regular.copyWith(fontSize: getW(14),color:AppColor.font),),
-                ],
-              ),
-              SizedBox(height: getH(10)),
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(10, (index) => const Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: CourseCard(),
-                  ),),
+            const Divider(thickness: 1),
+
+            Expanded(child:todoList.isEmpty? const Center(child: Text("Empty Todo"),):
+            ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount:todoList.length,
+                itemBuilder: (context,index){
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text(index.toString()),
                 ),
-              ),
-              SizedBox(height: getH(20)),
-            ],
-          ),
-        )
+
+                title:Text(todoList[index]['title'],style: AppFont.large.copyWith(
+                  fontSize: getW(18)
+                ),),
+                subtitle: Text(DateFormat.yMMMd().format(todoList[index]['date'])),
+
+                trailing: IconButton.filledTonal(
+                    onPressed: (){todoList.removeAt(index);setState(() {});},
+                    icon: const Icon(Icons.delete,color: Colors.red,)),
+              );
+            })
+            ),
+          ],
+        ),
       ),
     );
   }
+}
 
-  AppBar buildAppBar() {
-    return AppBar(
-      backgroundColor:AppColor.primary,
-      elevation: 0,
-      titleSpacing: 0,
-      leading: Padding(
-        padding: EdgeInsets.all(getW(10)),
-        child: CircleAvatar(
-          radius: getW(25),
-          backgroundColor: const Color(0xfff5f3f4),
-          backgroundImage: const NetworkImage(AppString.profile),
-        ),
-      ),
-      title: Text(AppString.appName,style: AppFont.large.copyWith(color:AppColor.font)),
+class DeleteBox extends StatelessWidget {
+  const DeleteBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Icon(Icons.info_outline),
+      content:const Text("Are you sure todo confirm delete"),
       actions: [
-        RoundIcon(icon: SvgPicture.asset(AppIcon.search,height:getH(20),width:getW(20)), press: () { },),
-        SizedBox(width: getW(15)),
-        RoundIcon(icon: SvgPicture.asset(AppIcon.notification,height:getH(20),width:getW(20)), press: () { },),
-        SizedBox(width: getW(10)),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(elevation: 0),
+            onPressed: (){},
+            child: const Text("Cancel")),
+
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(elevation: 0,backgroundColor: Colors.redAccent),
+            onPressed: (){},
+            child: const Text("Delete"))
       ],
     );
   }
 }
 
-class CourseCard extends StatelessWidget {
-  const CourseCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(10)
-      ),
-      height: getH(270),
-      width: getW(300),
-      child: Column(
-        children: [
-          ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10)
-              ),
-              child: Image.network("https://cdn.ostad.app/course/photo/2023-05-27T09-42-50.178Z-Website%20Image%20(27).jpg",fit: BoxFit.cover,)),
-
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: getW(5),vertical: getH(5)),
-                  child: Container(
-                      decoration:BoxDecoration(
-                        color: Colors.grey.shade300,
-                        borderRadius: BorderRadius.circular(2)
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: getW(5),vertical: getH(5)),
-                      child: Text("New Batch Will Start",style: AppFont.regular.copyWith(fontSize:getW(10)),)),
-                ),
-
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: getW(5),vertical: getH(5)),
-                  child: Text("App Development with Flutter",maxLines: 2,
-                    style: AppFont.medium.copyWith(fontSize:getW(18)),),
-                ),
-
-                const Spacer(),
-                Container(
-                  height: getH(40),
-                    width: double.infinity,
-                    decoration:BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius: const BorderRadius.only(
-                          bottomRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                        )
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text("ট10000",style: AppFont.regular.copyWith(fontSize:getW(14),decoration: TextDecoration.lineThrough),),
-                        Text("ট5000",style: AppFont.regular.copyWith(fontSize:getW(14)),),
-                      ],
-                    )),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Enrolled extends StatelessWidget {
-  const Enrolled({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children:List.generate(10, (index) => const Padding(
-          padding: EdgeInsets.only(right: 10),
-          child: SpecialCard(),
-        )),
-      ),
-    );
-  }
-}
 
 
-class SpecialCard extends StatelessWidget {
-  const SpecialCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 120,
-      child: Column(
-        children: [
-          Container(
-            height: getH(100),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(getW(5)),
-              color: Colors.blueGrey,
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                  image: NetworkImage("https://cdn.ostad.app//course/photo/2023-03-29T06-44-24.054Z-flutter%20main%20course.jpeg"))
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: getW(5),vertical: getH(5)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Batch 4",style: AppFont.regular.copyWith(fontSize:getW(10)),),
-                SizedBox(height: getH(5)),
-                Text("App Development with Flutter",maxLines: 2,
-                  style: AppFont.medium.copyWith(fontSize:getW(9)),),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
